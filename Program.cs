52f551c15a5f4d09614e560af4e847d09cc09eee
GritDim <gritsun.d@gmail.com>
2021-11-22 00:41:19 +0300

    test

--- Program.cs
+++ Program.cs
@@ -10,6 +10,19 @@ namespace Task4._1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите целочисленное число N>0");
+            int N = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1, N2 = 0; i <= N; i++)
+            {
+                N2 += i * 2 - 1;
+                Console.WriteLine("Значение N2 = {0}", N2);
+            }
+
+
+            Console.WriteLine("Нажмите любую клавишу");
+            Console.ReadKey();
+
+            
         }
     }
 }
